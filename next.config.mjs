30045d675654a162685e68fa61d89c8f5cefbd70
AnnRozhkina <annrozhkina@gmail.com>
2024-05-20 21:26:12 +0300

    create sprite

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,42 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import SVGSpritemapPlugin from 'svg-spritemap-webpack-plugin';
+import fs from 'fs-extra';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new SVGSpritemapPlugin('public/icons/*.svg', {
+          output: {
+            filename: 'static/sprites/icons.svg',
+            svgo: true,
+          },
+          sprite: {
+            prefix: false,
+          },
+        })
+      );
+
+      // Добавляем шаг копирования после сборки
+      config.plugins.push({
+        apply: (compiler) => {
+          compiler.hooks.afterEmit.tap('CopySvgPlugin', () => {
+            const src = join(__dirname, '.next/static/sprites/icons.svg');
+            const dest = join(__dirname, 'public/static/sprites/icons.svg');
+            fs.ensureDirSync(join(__dirname, 'public/static/sprites'));
+            fs.copyFileSync(src, dest);
+          });
+        },
+      });
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
