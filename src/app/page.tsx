1bf4cc3944c8f6114fb54c9c79659e916ca6f3c0
AnnRozhkina <annrozhkina@gmail.com>
2024-05-21 00:43:37 +0300

    create cards with svg icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import Image from "next/image";
-import Link from "next/link";
-import Icon from "./components/Icon";
+
+import { data } from '../../data'
+import Card from "./components/Card";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Icon name="door" className="icon-class" />
-      <Icon name="Electrical" className="icon-class" />
+    <main className="max-w-[1320px]  mx-auto my-7">
+      <h1 className='text-5xl text-center mb-10'>Cards with icons from svg sprite</h1>
+      <div className='text-2xl mb-8 mx-5'>
+        <p className='font-semibold'>Technologies used:</p>
+        <p className='mb-5'>Next.js TailwindCSS</p>
+        <p className='font-semibold'>Npm packages:</p>
+        <p>svg-sprite svg-spritemap-webpack-plugin</p>
+      </div>
+      <div className='flex flex-wrap justify-center sm:justify-around gap-[24px]'>
+        {data.map((item) => <Card key={item.id} item={item} />)}
+
+      </div>
     </main>
   );
 }
